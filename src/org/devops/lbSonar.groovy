18225644918b5387d2fede9de2e1766ac7d760a4
Antolinez17 <48690314+Az0917@users.noreply.github.com>
2023-12-26 09:48:47 -0500

    Update lbSonar.groovy

--- src/org/devops/lbSonar.groovy
+++ src/org/devops/lbSonar.groovy
@@ -2,11 +2,18 @@ package org.devops
 
 def analisisSonar(){
     def scannerHome = tool 'sonnar'
-    withSonarQubeEnv('sonnar') {
-        sh "${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=NodeJSApp \
-            -Dsonar.projectName=NodeJSApp \
-            -Dsonar.sources=./ \
-            -Dsonar.javascript.lcov.reportPaths=./coverage/locov.info "
+    if(scannerTool){
+        withSonarQubeEnv('sonnar'){
+            sh "${scannerHome}/bin/sonnar \
+            -Dsonar.projectKey='NodeJSApp' \
+            -Dsonar.projectName='NodeJSApp' \
+            -Dsonar.sources=src \
+            -Dsonar.tests=src/_test_ \
+            -Dsonar.exclusions=src/_test_/** \
+            -Dsonar.testExecutionReportPaths=./test-report.xml \
+            -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info"
+        }
+    } else{
+        error 'SonarQube Scanner not found'
     }
 }
