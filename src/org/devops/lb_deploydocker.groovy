a7f57e2898dc9b8f306bc2b8af0c4bc8601b6f2b
Antolinez17 <48690314+Az0917@users.noreply.github.com>
2024-01-03 14:49:48 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,6 +1,6 @@
 package org.devops
 def call (){
-  withEnv(["USER=daz2023","REPONAME=nodejsapp","BUILD_ID=latest"]){
+  withEnv(["USER=daz2023","repoName=nodejsapp","BUILD_ID=latest"]){
     sh 'docker compose up -d'
   }
 }
